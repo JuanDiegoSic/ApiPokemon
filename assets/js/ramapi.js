6d922266119d1d1cfc7eea5d7061c8a6fa5a2001
JuanDiegoSic <diego23jdsc@gmail.com>
2021-08-02 15:15:35 -0500

    Se le agregan imagenes y algunas correcciones

--- assets/js/ramapi.js
+++ assets/js/ramapi.js
@@ -6,41 +6,64 @@ const getData = (url) => {
   return fetch(url)
     .then((response) => response.json())
     .then((json) => {
-      llenarDatos(json.results),
-      paginacion(json.previous,json.next),
+      pokeData(json.results),
+      paginacion(json);
 
-      console.log(json.results,json.previous,json.next);
     })
     .catch((error) => {
       console.log("Error: ", error);
     });
 };
 
-// Dibujar cards de personajes
-const llenarDatos = (data) => {
-  let html = "";
-  data.forEach((pj) => {
-    html += '<div class="col mt-5">';
-    html += '<div class="card" style="width: 10rem;">';
-    html += '<div class="card-body">';
-    html += `<h5 class="card-title">${pj.name}</h5>`;
-    html += "</div>";
-    html += "</div>";
-    html += "</div>";
+// const pokedata
+
+const pokeData = (data) => {
+let html = "";
+document.getElementById("datosPersonajes").innerHTML = "";
+data.forEach((pj) => {
+  const URL = pj.url;
+  return fetch(URL)
+   .then((response) => response.json())
+   .then((json) => {
+    llenarDatos(json, html);
+   })
+   .catch((error) => {
+    console.log("Error: ", error);
   });
-  document.getElementById("datosPersonajes").innerHTML = html;
+});
+
+}
+
+// Dibujar cards de personajes
+const llenarDatos = (data,html) => {
+
+
+  html += '<div class="col mt-5">';
+  html += '<div class="card" style="width: 10rem;">';
+  html += `<img src="${data.sprites.other.dream_world.front_default}" class="card-img-top" alt="${data.name}"></img>`;
+  html += '<div class="card-body">';
+  html += `<h5 class="card-title">${data.name}</h5>`;
+  html += `<p class="card-text">Status: ${data.weight}</p>`;
+  html += `<p class="card-text">Specie: ${data.height}</p>`;
+  html += "</div>";
+  html += "</div>";
+  html += "</div>";
+
+  document.getElementById("datosPersonajes").innerHTML += html;
 };
 
 //Paginacion
-const paginacion = (previous,next) => {
+const paginacion = (data) => {
   let prevDisabled = "";
   let nextDisabled = "";
 
+  console.log(data.previous);
+
   let html = `<li class="page-item ${
-    previous == null ? (prevDisabled = "disabled") : (prevDisabled = "")
-  }"><a class="page-link" onclick="getData('${previous}')"> Previous </a></li> <li class="page-item ${
-    next == null ? (nextDisabled = "disabled") : (nextDisabled = "")
-  }"><a class="page-link" onclick="getData('${next}')"> Next </a></li>`;
+    data.previous == null ? (prevDisabled = "disabled") : (prevDisabled = "")
+  }"><a class="page-link" onclick="getData('${data.previous}')"> Previous </a></li> <li class="page-item ${
+    data.next == null ? (nextDisabled = "disabled") : (nextDisabled = "")
+  }"><a class="page-link" onclick="getData('${data.next}')"> Next </a></li>`;
 
   document.getElementById("paginacion").innerHTML = html;
 };
